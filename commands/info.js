6d51fdcc656bff8a3d1188b7a2041bc42a69bdda
StateCollegeCONELRAD <74683653+StateCollegeCONELRAD@users.noreply.github.com>
2020-12-31 14:12:31 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -8,7 +8,7 @@ module.exports = {
         .setColor('#49B6CE')
         .setTitle('log1x_')
         .setURL('https://www.twitch.tv/log1x_')
-        .setDescription('log1x_ is a Twitch Streamer with a Youtube Channel as well. He plays several games such as Minecraft, Cities: Skylines, Universe Sandbox, Portal & Portal 2, as well as many more. You can find him on Twitch at https://www.twitch.tv/log1x_ or on Youtube at https://www.youtube.com/channel/UCZss6e6RHoyfeRdafxbkZvA.')
+        .setDescription('log1x\_ is a Twitch Streamer with a Youtube Channel as well. He plays several games such as Minecraft, Cities: Skylines, Universe Sandbox, Portal & Portal 2, as well as many more. You can find him on Twitch at https://www.twitch.tv/log1x_ or on Youtube at https://www.youtube.com/channel/UCZss6e6RHoyfeRdafxbkZvA.')
         .setImage('https://log1x.us.to/images/log1x_-logo-transparent.png');
 
         message.channel.send(newEmbed);
