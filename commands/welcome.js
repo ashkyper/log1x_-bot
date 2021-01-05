1345299b013293de76298e80f01d893ebb2b9651
StateCollegeCONELRAD <74683653+StateCollegeCONELRAD@users.noreply.github.com>
2021-01-05 18:51:20 -0500

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -2,10 +2,10 @@ const { DiscordAPIError } = require("discord.js");
 const Canvas = require("canvas");
 
 module.exports = {
-    name: 'autopublish',
-    description: 'Automatically publishes a message',
+    name: 'welcome',
+    description: 'Automatically give role on join',
     async execute(guildMember, Discord) {
-		let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'memeber');
+		let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Memeber');
 
 		guildMember.roles.add(welcomeRole);
 		
